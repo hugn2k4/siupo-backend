8d09a915fc126b4c5ba3f2c8c4c9fc66db42c05d
Hung Le Cong <hcl2k4@gmail.com>
2025-10-18 01:36:41 +0700

    feat: thêm chức năng quên mật khẩu

--- src/main/java/com/siupo/restaurant/service/authentication/AuthenticationService.java
+++ src/main/java/com/siupo/restaurant/service/authentication/AuthenticationService.java
@@ -1,21 +1,16 @@
 package com.siupo.restaurant.service.authentication;
 
-import com.siupo.restaurant.dto.request.LoginRequest;
-import com.siupo.restaurant.dto.request.RegisterRequest;
-import com.siupo.restaurant.dto.request.RefreshTokenRequest;
-import com.siupo.restaurant.dto.request.LogoutRequest;
+import com.siupo.restaurant.dto.request.*;
 import com.siupo.restaurant.dto.response.LoginDataResponse;
 import com.siupo.restaurant.dto.response.MessageDataReponse;
 
 public interface AuthenticationService {
     LoginDataResponse login(LoginRequest loginRequest);
-
     MessageDataReponse register(RegisterRequest registerRequest);
     MessageDataReponse confirmRegistration(String email, String otp);
-
+    MessageDataReponse requestForgotPassword(String email);
+    MessageDataReponse setNewPassword(ForgotPasswordRequest forgotPasswordRequest);
     void resendOtp(String email);
-    
     LoginDataResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
-    
     void logout(LogoutRequest logoutRequest);
 }
