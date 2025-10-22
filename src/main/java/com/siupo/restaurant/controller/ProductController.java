880ac42b34d23ca824d2a72a27eaa1d25e3dee8f
Hung Le Cong <hcl2k4@gmail.com>
2025-10-22 22:03:03 +0700

    fix: fix auth product

--- src/main/java/com/siupo/restaurant/controller/ProductController.java
+++ src/main/java/com/siupo/restaurant/controller/ProductController.java
@@ -3,10 +3,12 @@ package com.siupo.restaurant.controller;
 import com.siupo.restaurant.dto.ProductDTO;
 import com.siupo.restaurant.dto.response.ApiResponse;
 import com.siupo.restaurant.enums.EProductStatus;
+import com.siupo.restaurant.model.User;
 import com.siupo.restaurant.service.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
