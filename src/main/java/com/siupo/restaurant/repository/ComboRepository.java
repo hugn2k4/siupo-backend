11c842eebb5cb4a4a42859e43422fcc6d098eab4
Hùng Lê <118160879+hugn2k4@users.noreply.github.com>
2025-12-07 14:27:47 +0700

    Feature/combo (#34)
    * feat: api combo
    
    * feature combo

---
+++ src/main/java/com/siupo/restaurant/repository/ComboRepository.java
@@ -0,0 +1,18 @@
+package com.siupo.restaurant.repository;
+
+import com.siupo.restaurant.enums.EProductStatus;
+import com.siupo.restaurant.model.Combo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ComboRepository extends JpaRepository<Combo, Long> {
+    
+    List<Combo> findByStatus(EProductStatus status);
+    
+    @Query("SELECT c FROM Combo c LEFT JOIN FETCH c.items WHERE c.id = :id")
+    Combo findByIdWithItems(Long id);
+}
