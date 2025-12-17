f066dcc1f0e9cbb7edc593365a3152b67ddd70de
Hung Le Cong <hcl2k4@gmail.com>
2025-12-17 17:52:31 +0700

    refactor: clean and fix code category

---
+++ src/main/java/com/siupo/restaurant/dto/request/CategoryRequest.java
@@ -0,0 +1,13 @@
+package com.siupo.restaurant.dto.request;
+
+import com.siupo.restaurant.dto.ImageDTO;
+import lombok.Data;
+
+@Data
+public class CategoryRequest {
+    private Long id;
+    private String name;
+    private String imageUrl;
+    private String imageName;
+    private ImageDTO image;
+}
